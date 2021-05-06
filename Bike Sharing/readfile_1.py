d4ddb7579bbcfef3b7504d4377982e26a8af3b45
Ngoc Tran <newbiettn@gmail.com>
2021-05-07 07:58:13 +1000

    add Bike Sharing

---
+++ Bike Sharing/readfile_1.py
@@ -0,0 +1,18 @@
+import csv
+import os
+
+local_path = os.getcwd()
+SOURCE = 'Bike Sharing/datasets/hour.csv'
+SEP = ','
+
+with open(local_path + '/' + SOURCE) as R:
+    csv_reader = csv.reader(R, delimiter=SEP)
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+            header = row
+        else:
+            line_count += 1
+    print('Total rows: %i' % line_count)
