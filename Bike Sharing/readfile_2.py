6bb05c6462c16573991fb3093003f072391b63cd
Ngoc Tran <newbiettn@gmail.com>
2021-05-07 08:19:01 +1000

    add file

--- Bike Sharing/readfile_2.py
+++ Bike Sharing/readfile_2.py
@@ -1,3 +1,19 @@
+################################################################################
+# readfile_2.py
+# AUTHOR:           NGOC TRAN
+# CREATED:          07 May 2021
+# DESCRIPTION:      Stream data from CSV file using pandas lib.
+################################################################################
 import pandas as pd
+import os
 
-CHUNK_SIZE = 1000
+local_path = os.getcwd()
+SOURCE = 'Bike Sharing/datasets/hour.csv'
+CHUNK_SIZE = 1000  # number of rows has to return at each iteration
+
+with open(local_path + '/' + SOURCE) as stream:
+    iterator = pd.read_csv(stream, chunksize=CHUNK_SIZE)
+    for row in iterator:
+        print ('Size of uploaded chunk: %i instance, %i features' %(row.shape))
+    print ('Sample values:  \n%s' % str(row.iloc[0]))
+
